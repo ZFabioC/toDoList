35b9f3103b4d8dd9d935ca75871652764ae52307
ZFabioC <fabiozanella00@gmail.com>
2023-07-12 16:38:02 -0300

    implementado a busca de todos

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const formAddTodo = document.querySelector('.form-todo')
 const todosContainer = document.querySelector('.content-todos')
-
+const searchTodos = document.querySelector('.form-search input')
 
 formAddTodo.addEventListener('submit', event => {
     event.preventDefault()
@@ -8,7 +8,7 @@ formAddTodo.addEventListener('submit', event => {
     const inputValue = event.target.inputTodo.value.trim()
     if (inputValue.length) {
         todosContainer.innerHTML += `
-            <li>
+            <li class="content-li">
                 <span>${inputValue}</span>
                 <img class="trash-icon" src="./assets/recycle-bin-icon.png">
             </li>
@@ -25,4 +25,22 @@ todosContainer.addEventListener('click', event => {
     if (Array.from(clickedElement.classList).includes('trash-icon')) {
         clickedElement.parentElement.remove()
     }
+})
+
+searchTodos.addEventListener('input', event => {
+    const inputValue = event.target.value.trim().toLowerCase()
+    
+    Array.from(todosContainer.children)
+        .filter(todo => !todo.textContent.toLowerCase().includes(inputValue))
+        .forEach(todo => {
+            todo.classList.remove('content-li')
+            todo.classList.add('hidden')
+    })
+
+    Array.from(todosContainer.children)
+        .filter(todo => todo.textContent.toLowerCase().includes(inputValue))
+        .forEach(todo => {
+            todo.classList.remove('hidden')
+            todo.classList.add('content-li')
+    })
 })
