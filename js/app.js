d4a933da7b8be03079218f96ae0a32ccb6d00d60
ZFabioC <fabiozanella00@gmail.com>
2023-07-18 09:03:21 -0300

    Código refatorado

--- js/app.js
+++ js/app.js
@@ -2,45 +2,74 @@ const formAddTodo = document.querySelector('.form-todo')
 const todosContainer = document.querySelector('.content-todos')
 const searchTodos = document.querySelector('.form-search input')
 
-formAddTodo.addEventListener('submit', event => {
-    event.preventDefault()
+const showTodo = inputValue => {
+    todosContainer.innerHTML += `
+        <li class="content-li">
+            <span>${inputValue}</span>
+            <img class="trash-icon" src="./assets/recycle-bin-icon.png">
+        </li>
+    `
+}
 
+const resetInputValue = event => {
+    event.target.reset()
+}
+
+const addTodo = event => {
+    event.preventDefault()
     const inputValue = event.target.inputTodo.value.trim()
-    if (inputValue.length) {
-        todosContainer.innerHTML += `
-            <li class="content-li">
-                <span>${inputValue}</span>
-                <img class="trash-icon" src="./assets/recycle-bin-icon.png">
-            </li>
-        `
-        event.target.reset()
+    const isValueValid = inputValue.length 
+
+    if (isValueValid) {
+        showTodo(inputValue)
+        resetInputValue(event)
         return
     }
-    
     alert('É necessário dar um nome a tarefa!')
-})
+}
 
-todosContainer.addEventListener('click', event => {
+const convertInArray = domList => Array.from(domList)
+
+const removeTodo = event => {
     const clickedElement = event.target
-    if (Array.from(clickedElement.classList).includes('trash-icon')) {
+    const classesOfClickedElement = convertInArray(clickedElement.classList)
+    const isATrashIcon = classesOfClickedElement.includes('trash-icon')
+
+    if (isATrashIcon) {
         clickedElement.parentElement.remove()
     }
-})
+}
 
-searchTodos.addEventListener('input', event => {
-    const inputValue = event.target.value.trim().toLowerCase()
-    
-    Array.from(todosContainer.children)
-        .filter(todo => !todo.textContent.toLowerCase().includes(inputValue))
+const hideTodos = (todos, value) => {
+    todos
+        .filter(todo => !todo.textContent.toLowerCase().includes(value))
         .forEach(todo => {
             todo.classList.remove('content-li')
             todo.classList.add('hidden')
     })
+}
 
-    Array.from(todosContainer.children)
-        .filter(todo => todo.textContent.toLowerCase().includes(inputValue))
+const showTodos = (todos, value) => {
+    todos
+        .filter(todo => todo.textContent.toLowerCase().includes(value))
         .forEach(todo => {
             todo.classList.remove('hidden')
             todo.classList.add('content-li')
     })
-})
+}
+
+
+const searchTodo = event => {
+    const inputValue = event.target.value.trim().toLowerCase()
+    const todos = convertInArray(todosContainer.children)
+
+    hideTodos(todos, inputValue)
+    showTodos(todos, inputValue)
+        
+}
+
+
+
+formAddTodo.addEventListener('submit', addTodo)
+todosContainer.addEventListener('click', removeTodo)
+searchTodos.addEventListener('input', searchTodo)
